cd6308061e9bc6a53a35254b9cd0bea53cf74973
Maarten Engels <maartene@mac.com>
2022-01-26 08:45:30 +0100

    make CORS policy optional

--- stw-be/Sources/App/configure.swift
+++ stw-be/Sources/App/configure.swift
@@ -11,14 +11,16 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    let corsConfiguration = CORSMiddleware.Configuration(
-        allowedOrigin: .all,
-        allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
-        allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent, .accessControlAllowOrigin]
-    )
-    let cors = CORSMiddleware(configuration: corsConfiguration)
-    // cors middleware should come before default error middleware using `at: .beginning`
-    app.middleware.use(cors, at: .beginning)
+    if Environment.get("STW_SETUP_CORS") != nil {
+        let corsConfiguration = CORSMiddleware.Configuration(
+            allowedOrigin: .all,
+            allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+            allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent, .accessControlAllowOrigin]
+        )
+        let cors = CORSMiddleware(configuration: corsConfiguration)
+        // cors middleware should come before default error middleware using `at: .beginning`
+        app.middleware.use(cors, at: .beginning)
+    }
     
     let queuesDatabase: MongoDatabase
     
